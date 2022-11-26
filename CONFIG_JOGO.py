c75b1c7c1e3945ed33d851d1c186e7d0fc08a026
lujagon <luca.jagon@gmail.com>
2022-11-26 12:13:08 -0300

    voltando a branch

---
+++ CONFIG_JOGO.py
@@ -0,0 +1,16 @@
+
+class ConfigJogo:
+
+
+    LARGURA_TELA = 600
+    ALTURA_TELA = 400
+    COR_TITULO = (0, 0, 0)
+    FONTE_TITULO = 72
+    FONTE_SUBTITULO = 36
+    TELA = 1
+    Fonte_HISTORIA=25
+    VELOCIDADE_x=1
+    VELOCIDADE_y=1
+    DURACAO_PARTIDA=60
+    tamanho_per=50
+    meiotamanho_per=25
