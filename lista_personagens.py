810c2a26277a876a388701e092a3f492b042e38c
lujagon <luca.jagon@gmail.com>
2022-12-11 15:44:23 -0300

    velocidade dos personagens

--- lista_personagens.py
+++ lista_personagens.py
@@ -12,28 +12,20 @@ class stats:
     def __init__(self):
         self.tiro=tirinho()
         self.ataque_perto=fisico()
-        self.soldado_1=Personagem("soldado", 300, .5, 20, 0,  (self.ataque_perto))
-        self.soldado_2=Personagem("soldado", 300, .5, 20, 0,  (self.ataque_perto))
-        self.soldado_3=Personagem("soldado", 300, .5, 20, 0,  (self.ataque_perto))
+        self.soldado_1=Personagem("soldado", 300, 1, 20, 0,  (self.ataque_perto))
+        self.soldado_2=Personagem("soldado", 300, 1, 20, 0, (self.ataque_perto))
+        self.soldado_3=Personagem("soldado", 300, 1, 20, 0,  (self.ataque_perto))
         self.ataque_invocar=minion([self.soldado_1, self.soldado_2, self.soldado_3])
-        self.STALIN=Personagem("STALIN", 2000, 0.8, 35, 150, (self.ataque_invocar, magia()))
-        self.GORBACHOV=Personagem("GORBACHOV", 3500, 0.2, 40, 0, (self.ataque_perto, magia()))
-        self.IVAN_DRAGO=Personagem("IVAN DRAGO", 3000, 1, 40, 0, (self.ataque_perto, self.tiro))
-        self.RASPUTIN=Personagem("RASPUTIN", 1500, 1, 2, 190, (cura(), magia()))
-        self.RAMBO=Personagem("RAMBO", 3000, 1.2, 40, 0,(self.ataque_perto, self.tiro))
+        self.STALIN=Personagem("STALIN", 2000, 2, 35, 150, (self.ataque_invocar, magia()))
+        self.GORBACHOV=Personagem("GORBACHOV", 2900, 1.5, 40, 0, (self.ataque_perto, magia()))
+        self.IVAN_DRAGO=Personagem("IVAN DRAGO", 3000, 2.5, 40, 0, (self.ataque_perto, self.tiro))
+        self.RASPUTIN=Personagem("RASPUTIN", 1500, 1.5, 2, 220, (cura(), magia()))
+        self.RAMBO=Personagem("RAMBO", 3000, 2.5, 40, 0,(self.ataque_perto, self.tiro))
         self.MUHAMMAD_ALI=Personagem("MUHAMMAD ALI", 1800, 3, 50, 0,(self.ataque_perto, dash()))
-        self.JESSE_JAMES=Personagem("JESSE JAMES", 2800, 1.2, 50, 120,  (self.ataque_perto, self.tiro))
-        self.JOHN_F_KENNEDY=Personagem("JOHN F KENNEDY", 1000, .8, 50, 30, (self.ataque_invocar, magia()))
-        self.STALIN_=Personagem("STALIN", 2000, 0.8, 35, 150, (self.ataque_invocar, magia()))
-        self.GORBACHOV_=Personagem("GORBACHOV", 3500, 0.2, 40, 0, (self.ataque_perto, magia()))
-        self.IVAN_DRAGO_=Personagem("IVAN DRAGO", 3000, 1, 40, 0, (self.ataque_perto, self.tiro))
-        self.RASPUTIN_=Personagem("RASPUTIN", 1500, 1, 2, 190, (cura(), magia()))
-        self.RAMBO_=Personagem("RAMBO", 3000, 1.2, 40, 0,(self.ataque_perto, self.tiro))
-        self.MUHAMMAD_ALI_=Personagem("MUHAMMAD ALI", 1800, 3, 50, 0,(self.ataque_perto, dash()))
-        self.JESSE_JAMES_=Personagem("JESSE JAMES", 2800, 1.2, 50, 120,  (self.ataque_perto, self.tiro))
-        self.JOHN_F_KENNEDY_=Personagem("JOHN F KENNEDY", 1000, .8, 50, 30, (self.ataque_invocar, magia()))
+        self.JESSE_JAMES=Personagem("JESSE JAMES", 2800, 2.5, 50, 120,  (self.ataque_perto, self.tiro))
+        self.JOHN_F_KENNEDY=Personagem("JOHN F KENNEDY", 1000, 1.5, 50, 30, (self.ataque_invocar, magia()))
         self.lista_3 = [self.STALIN, self.GORBACHOV , self.IVAN_DRAGO, self.RASPUTIN, self.MUHAMMAD_ALI, self.JESSE_JAMES, self.RAMBO, self.JOHN_F_KENNEDY]
-
+       
     def reseta_tudo(self):
         self.tiro=tirinho()
         self.ataque_perto=fisico()
@@ -43,13 +35,14 @@ class stats:
         self.ataque_invocar=minion([self.soldado_1, self.soldado_2, self.soldado_3])
         self.STALIN=Personagem("STALIN", 2000, 2, 35, 150, (self.ataque_invocar, magia()))
         self.GORBACHOV=Personagem("GORBACHOV", 2900, 1.5, 40, 0, (self.ataque_perto, magia()))
-        self.IVAN_DRAGO=Personagem("IVAN DRAGO", 3000, 2.5, 40, 0, (self.ataque_perto, self.tiro()))
+        self.IVAN_DRAGO=Personagem("IVAN DRAGO", 3000, 2.5, 40, 0, (self.ataque_perto, self.tiro))
         self.RASPUTIN=Personagem("RASPUTIN", 1500, 1.5, 2, 220, (cura(), magia()))
         self.RAMBO=Personagem("RAMBO", 3000, 2.5, 40, 0,(self.ataque_perto, self.tiro))
         self.MUHAMMAD_ALI=Personagem("MUHAMMAD ALI", 1800, 3, 50, 0,(self.ataque_perto, dash()))
         self.JESSE_JAMES=Personagem("JESSE JAMES", 2800, 2.5, 50, 120,  (self.ataque_perto, self.tiro))
         self.JOHN_F_KENNEDY=Personagem("JOHN F KENNEDY", 1000, 1.5, 50, 30, (self.ataque_invocar, magia()))
         self.lista_3 = [self.STALIN, self.GORBACHOV , self.IVAN_DRAGO, self.RASPUTIN, self.MUHAMMAD_ALI, self.JESSE_JAMES, self.RAMBO, self.JOHN_F_KENNEDY]
+       
        
         
 
